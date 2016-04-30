b593cbe10af97e88729de0e7674d57cc29148150
Michael Gattozzi <mgattozzi@gmail.com>
2016-04-30 18:06:01 -0400

    Schema Change
    Adds ubuntu-dev packages to schema

--- migrations/20160428112233_packages_table/up.sql
+++ migrations/20160428112233_packages_table/up.sql
@@ -3,10 +3,10 @@ CREATE TABLE packages (
   id SERIAL PRIMARY KEY,
   arch TEXT NOT NULL,
   aur TEXT NOT NULL,
-  ubuntu TEXT NOT NULL
+  ubuntu TEXT NOT NULL,
+  ubuntu_dev TEXT NOT NULL
 );
 
 GRANT SELECT ON packages TO root;
 
-INSERT INTO packages(arch, aur, ubuntu) VALUES ('sudo','','sudo');
-INSERT INTO packages(arch, aur, ubuntu) VALUES ('sudo','','hello');
+INSERT INTO packages(arch, aur, ubuntu, ubuntu_dev) VALUES ('sudo','','sudo','');
--- src/shaper/models.rs
+++ src/shaper/models.rs
@@ -3,5 +3,6 @@ pub struct Package {
     pub id: i32,
     pub arch: String,
     pub aur: String,
-    pub ubuntu: String
+    pub ubuntu: String,
+    pub ubuntu_dev: String
 }
