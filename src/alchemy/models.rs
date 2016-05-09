43d2c6e9732438fa1e090a2d661848ea1391cff6
Michael Gattozzi <mgattozzi@gmail.com>
2016-05-08 20:59:30 -0400

    Fix Master Merge Error
    Let it be stated here that from now on out the project won't be dealing
    with stuff like this because I set the protected branches to work
    properly now

--- src/alchemy/models.rs
+++ src/alchemy/models.rs
@@ -1,5 +1,4 @@
 #[derive(Queryable, Debug)]
-<<<<<<< HEAD
 /// Struct for a row in the Alchemist package table
 pub struct Package {
     /// Row Number makes it easy for migration
@@ -11,12 +10,5 @@ pub struct Package {
     /// Ubuntu Binary Packages
     pub ubuntu: String,
     /// Ubuntu Development Header Packages
-=======
-pub struct Package {
-    pub id: i32,
-    pub arch: String,
-    pub aur: String,
-    pub ubuntu: String,
->>>>>>> master
     pub ubuntu_dev: String
 }
