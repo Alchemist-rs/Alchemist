7339a439add28fefd2ee4be9f3ae431fcb34323c
Michael Gattozzi <mgattozzi@gmail.com>
2016-05-10 22:28:29 -0400

    Alchemist v0.2.0 - SQLite/HashMap breakage
    Due to the fact that I rewrote everything to use sqlite rather than
    using postgresql I'm bumping the version to v0.2.0 rather than doing
    a minor bump. The benefit of this though is that many people already
    have sqlite installed as a dependency on their system. The other benefit
    of it is that it's much lighter on the system in terms of resources
    making it real nice for a command line tool like Alchemist. The jump to
    v0.2.0 makes sense because everything was switched from Vecs to HashSets
    breaking anyone using code based off this.
    
    Release Notes:
    
    - HashSets rather than Vecs for increased efficiency and fitting the needs
      of how Alchemist works
    - SQLite rather than Postgresql for lighter dev dependencies and less
      resources being used on the system.
    
    Upgrade

--- scripts/arch_setup.sh
+++ scripts/arch_setup.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-#Install depdendencies and set them up
-#sqlite is only included to get diesel_cli working
-sudo pacman -S postgresql postgresql-libs
-echo "Enabling postgresql"
-sudo systemctl enable postgresql
-echo "Starting postgresql"
-sudo systemctl start postgresql
+#Install db depdendency
+sudo pacman -S sqlite3
 
 #Install multirust
 curl -sf https://raw.githubusercontent.com/brson/multirust/master/blastoff.sh | sh
@@ -17,9 +12,8 @@ multirust update nightly
 multirust override nightly
 
 #Install diesel_cli and db
-createdb alchemist
 cargo install diesel_cli
-~/.cargo/bin/diesel migration run
+~/.cargo/bin/diesel setup
 echo "Add .cargo/bin to your PATH if you want to use diesel_cli everywhere"
 
 echo "The project is all setup!"
--- src/alchemy/db.rs
+++ src/alchemy/db.rs
@@ -2,7 +2,7 @@ extern crate diesel;
 extern crate dotenv;
 
 use diesel::prelude::*;
-use diesel::pg::PgConnection;
+use diesel::sqlite::SqliteConnection;
 use dotenv::dotenv;
 use models::Package;
 use std::env;
@@ -10,14 +10,14 @@ use std::collections::HashSet;
 
 
 /// Establishes a connection to the Alchemist DB
-fn establish_connection() -> PgConnection {
+fn establish_connection() -> SqliteConnection {
     //Read from the .env file where the db is located
     //at so we can connect to it.
     dotenv().ok();
     let database_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set");
 
-    PgConnection::establish(&database_url)
+    SqliteConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
 }
 
@@ -33,30 +33,28 @@ fn establish_connection() -> PgConnection {
 /// ```
 ///
 pub fn pack_query(mut input_packages: HashSet<String>) -> HashSet<Package> {
-    //These allow us to use schema specific references
-    //as well as functions like eq(), or(), and any()
-    //in our querys using diesel
-    use diesel::expression::dsl::*;
     use schema::packages::dsl::*;
 
     let connection = establish_connection();
 
-    //TODO Establish a way for Diesel to accept a HashSet
-    let query_input = input_packages
-        .drain().collect::<Vec<String>>();
-
-    let query_output = packages.filter(
-        arch.eq(any(&query_input))
-            .or(aur.eq(any(&query_input)))
-            .or(ubuntu.eq(any(&query_input)))
-            .or(ubuntu_dev.eq(any(&query_input)))
-        )
-        .load::<Package>(&connection)
-        .expect("Error loading packages");
-
     let mut output: HashSet<Package> = HashSet::new();
-    for i in query_output {
-        output.insert(i);
+
+    for i in input_packages {
+        //While this might look like O(n^2) complexity
+        //it's more closer to O(n) since most querys only
+        //return one or 2 results really. Still this seemed
+        //to be the only way to implement it well
+        let results = packages.filter(
+            arch.eq(&i)
+            .or(aur.eq(&i))
+            .or(ubuntu.eq(&i))
+            .or(ubuntu_dev.eq(&i))
+        )
+        .get_results::<Package>(&connection)
+        .expect("Error loading packages"));
+        for j in results {
+            output.insert(i)
+        }
     }
 
     output
