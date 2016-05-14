cd6fa9fd499dd4513d6ac1f84762adb33c95ee29
Michael Gattozzi <mgattozzi@gmail.com>
2016-05-14 15:35:19 -0400

    One and Done Build Script
    Installs diesel_cli if not available on the system
    Sets up database if not available
    Then compiles like normal

---
+++ build.rs
@@ -0,0 +1,29 @@
+use std::process::Command;
+
+fn main() {
+    //Run which in order to check for availability
+    let which = String::from_utf8(Command::new("which")
+            .arg("diesel")
+            .output()
+            .unwrap()
+            .stderr)
+            .unwrap();
+
+    //If diesel_cli isn't installed install it for the user
+    if which.contains("no diesel in") {
+        let mut install_diesel = Command::new("cargo")
+            .arg("install")
+            .arg("diesel_cli")
+            .spawn()
+            .unwrap_or_else(|e| { panic!("Failed to execute: {}", e) });
+        install_diesel.wait()
+            .unwrap_or_else(|e| { panic!("Failed to execute: {}", e) });
+    }
+
+    //Run any migrations available that haven't been run before building
+    let _dump = Command::new("diesel")
+            .arg("migration")
+            .arg("run")
+            .output()
+            .unwrap_or_else(|e| { panic!("Failed to execute: {}", e) });
+}
