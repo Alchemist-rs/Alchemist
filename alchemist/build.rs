07d4fa81456a632def5f06a3af7f1cbfc99f7d47
LordDoctors <hoffmeyer25@gmail.com>
2017-02-25 18:52:58 -0500

    Fix the build script to not install diesel manually
    It was breaking travis by doing it the way it was formally. This way,
    diesel_cli is installed and managed by cargo, and weird checks to see
    if it needs to be installed don't have to be done.

--- alchemist/build.rs
+++ alchemist/build.rs
@@ -1,31 +1,25 @@
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
-    // Run which in order to check for availability
-    let which = String::from_utf8(Command::new("which")
-            .arg("diesel")
+    // If cross is installed its probs being built on travis, don't do anything
+    if Command::new("which").arg("cross").output().unwrap().status.success() {
+        println!("Cross is found on the sytem, not worrying about diesel/databases as this is \
+                  probably being run on Travis");
+        return;
+    } else {
+        // If the alchemist.db isn't existent, make one.
+        if Path::new("./alchemist.db").exists() == false {
+            println!("database not found, making one...");
+            Command::new("diesel").arg("setup");
+        }
+        // Run any migrations available that haven't been run before building
+        println!("Running Migrations...");
+        Command::new("diesel")
+            .arg("migration")
+            .arg("run")
             .output()
-            .unwrap()
-            .stderr)
-        .unwrap();
+            .unwrap_or_else(|e| panic!("Failed to execute: {}", e));
 
-    // If diesel_cli isn't installed install it for the user
-    if which.contains("no diesel in") || which.is_empty() {
-        let mut install_diesel = Command::new("cargo")
-            .arg("install")
-            .arg("diesel_cli")
-            .arg("--no-default-features")
-            .arg("--features=sqlite")
-            .spawn()
-            .unwrap_or_else(|e| panic!("Failed to execute: {}", e));
-        install_diesel.wait()
-            .unwrap_or_else(|e| panic!("Failed to execute: {}", e));
     }
-
-    // Run any migrations available that haven't been run before building
-    let _dump = Command::new("diesel")
-        .arg("migration")
-        .arg("run")
-        .output()
-        .unwrap_or_else(|e| panic!("Failed to execute: {}", e));
 }
