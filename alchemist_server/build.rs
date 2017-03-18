b1e4c46122f451e263f411dbb20d720459461de1
SirDoctors <hoffmeyer25@gmail.com>
2017-03-17 23:51:02 -0400

    Make changes suggested by rustfmt

--- alchemist_server/build.rs
+++ alchemist_server/build.rs
@@ -3,11 +3,11 @@ use std::process::Command;
 fn main() {
     // Run which in order to check for availability
     let which = String::from_utf8(Command::new("which")
-            .arg("diesel")
-            .output()
-            .unwrap()
-            .stderr)
-        .unwrap();
+                                      .arg("diesel")
+                                      .output()
+                                      .unwrap()
+                                      .stderr)
+            .unwrap();
 
     // If diesel_cli isn't installed install it for the user
     if which.contains("no diesel in") || which.is_empty() {
@@ -18,8 +18,7 @@ fn main() {
             .arg("--features=sqlite")
             .spawn()
             .unwrap_or_else(|e| panic!("Failed to execute: {}", e));
-        install_diesel.wait()
-            .unwrap_or_else(|e| panic!("Failed to execute: {}", e));
+        install_diesel.wait().unwrap_or_else(|e| panic!("Failed to execute: {}", e));
     }
 
     // Run any migrations available that haven't been run before building
